2f56736e436eb44da58400f658941718f7e774e1
Moroni Santos <moroni.santos@academico.ifpb.edu.br>
2021-05-18 17:28:31 -0300

    removendo arquivos que não fazem parte das listas

--- E1.1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Hello, World!"
-
-read -p "Qual o seu nome? " nome
-
-echo "prazer em te conhecer, ${nome}!, hoje é "
-date
--- E2.3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo $*
-echo $1
-echo $5
-echo ${12}
-echo ${13}
